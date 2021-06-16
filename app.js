9b879f1668caaf66374694f5c3002f51bcbb1d4d
Raghad98 <>
2021-06-16 13:29:48 +0000

    pop up boxes

---
+++ app.js
@@ -0,0 +1,15 @@
+var name = prompt("Welcome Queen to our online Shopping Journy , What's your name?")
+alert("Have fun Shopping "+ name)
+var x = prompt("tell us what are u intrested in from these(Dresses, Winter Fasion, Accessories)?")
+
+if(x == "dresses"){
+  document.write("<p>"+ "Perfect Choise , Who doesn't love dresses" + "</p>")}
+
+  else if(x == "winter fashion"){
+  document.write("<p>"+ "A maizing , let's try it Out" + "</p>")}
+
+else if(x == "accessories"){
+  document.write("<p>"+ "we have the greatest collection , let's check it out" + "</p>")} 
+
+else{
+  alert("sorry try to type it again")}
