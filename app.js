f41c732941a03e2dee5d12bce6050c5efca6fb1a
Raghad98 <>
2021-06-18 15:05:21 +0000

    new lists

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 var name = prompt("Welcome Queen to our online Shopping Journy , What's your name?")
 alert("Have fun Shopping "+ name)
-var x = prompt("tell us what are u intrested in from these(Dresses, Winter Fasion, Accessories)?")
+var x = prompt("tell us what are u intrested in from these(dresses, winter fasion, accessories)?")
 
 while (x != "dresses" && x != "winter fashion" && x != "accessories") {
 
-  x = prompt("you entered wrong movie please try gain ")
+  x = prompt(" Sorry ! you entered wrong input please try again ! ")
 }
 
-if(x == "dresses"){var numOfPic = prompt (" How many dresses do you want to see ?")
+if(x == "dresses"){var numofdress = prompt (" How many dresses do you want to see ?")
   }
-  for (var y = 1; y <= numOfPic; y++){document.write("<div>"+ "<h3>" + "dresses"+"</h3>"+"<img src=' https://cdn.shopify.com/s/files/1/0109/5516/1658/products/9_0623386b-dbd5-4ad5-943c-3c003cd9ca0b_1000x1000.jpg?v=1564283932'>"+"</div>") } 
+  for (var y = 1; y <= numofdress; y++){document.write("<div>"+ "<h3>" + "dresses"+"</h3>"+"<img src=' https://i.pinimg.com/originals/df/e0/37/dfe037e0c609fd10421157a61b978619.jpg'>"+"</div>") } 
 
-if(x == "winter fashion"){
-  document.write("<p>"+ "amaizing , get ready to be warm and stylish at the same time" + "</p>")} 
+if(x == "winter fashion"){var numofcoats = prompt (" How many coats do you want to see ?")
+  }
+  for (var y = 1; y <= numofcoats; y++){document.write("<div>"+ "<h3>" + "dresses"+"</h3>"+"<img src=' https://www.nydailynews.com/resizer/48P3Ss8IDd964K5GPhQ4Ti5UBUY=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/NJYYCMNHPZFOZPMHF2VLKNKGEQ.jpg'>"+"</div>") }
 
-if(x == "accessories"){
-  document.write("<p>"+ "we have the greatest collection let's check it out" + "</p>")} 
+  if(x == "accessories"){var numofitems = prompt (" How many item do you want to see ?")
+  }
+  for (var y = 1; y <= numofitems; y++){document.write("<div>"+ "<h3>" + "dresses"+"</h3>"+"<img src=' https://i.pinimg.com/originals/04/ce/2e/04ce2ed7c7fb8d7454fcbace300895ea.jpg'>"+"</div>") } 
 
-  
 
 
 
